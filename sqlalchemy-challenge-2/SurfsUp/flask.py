6b9bad272dab77c1e9294304cbebccbafdcaaff3
dvang3 <135193343+dvang3@users.noreply.github.com>
2023-08-28 18:48:55 -0700

    Add files via upload

--- sqlalchemy-challenge-2/SurfsUp/flask.py
+++ sqlalchemy-challenge-2/SurfsUp/flask.py
@@ -29,12 +29,24 @@ app = Flask(__name__)
 def hellow_world():
     return "Helllo World"
 
-@app.route("/api/v1")
+@app.route("/api/v1.0/precipitation")
 def app1():
          station_results = session.query(station.station).all()
 
          station = list(np.ravel(station_results))
          return jsonify(station = station)
+
+@app.route("/api/v1.0/stations")
+def station():
+       return jsonify(station)
+
+
+@app.route("/api/v1.0/tobs")
+def app1():
+
+@app.route("//pi/v1.0/<start> ")
+def app1():
+
 
 if __name__ == '__main__':
        app.run()
